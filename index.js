177a4c8214deb7e86823a296c642e80150da8ce7
Arjun <Arjun@code.com>
2022-11-29 09:31:03 +0530

    fix: output color

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ var notesReturn=document.querySelectorAll(".notes-return");
 var availableNotes=[2000,500,100,50,20,10,5,2,1]
 
 function validate(){
-    if(parseInt(cashGiven.value) <0 || parseInt(billAmount.value)<0){
-       showMsg.innerHTML="Amount should be positive";
+    console.log(cashGiven.value)
+    if(cashGiven.value ==NaN || cashGiven.value=="" || cashGiven.value===null ||billAmount.value ==NaN || billAmount.value=="" || billAmount.value===null){
+        showMsg.innerHTML="cannot be blank";
+        showMsg.style.color="red";
+        return false;
+    }
+    if(parseInt(cashGiven.value) <=0 || parseInt(billAmount.value)<=0){
+       showMsg.innerHTML="Amount should be positive or greater than zero";
+       showMsg.style.color="red";
         return false;
     }
 
     if(parseInt(cashGiven.value)< parseInt(billAmount.value)){
         showMsg.innerHTML="Cash given amount should be greater than Bill Amount"
+        showMsg.style.color="red";
         return false;
     }
     if(parseInt(cashGiven.value)== parseInt(billAmount.value)){
         showMsg.innerHTML="No change is required."
+        showMsg.style.color="red";
         return false;
     }
     showMsg.innerHTML="Below are the change details"
@@ -36,10 +45,11 @@ function calculateChange(amountReturn){
 }
 
 function eventHandler(){
+    
     if(!validate()){
         return false;
     };
-
+    showMsg.style.color="#a21caf";
     var amountReturn=parseInt(cashGiven.value)-parseInt(billAmount.value)
     calculateChange(amountReturn)
 }
